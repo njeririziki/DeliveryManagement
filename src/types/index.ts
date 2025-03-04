324dc8b8cb3005f913ee28a680f74af0f19ebaad
njeririziki <rizikinjeri@gmail.com>
2025-03-04 17:02:37 +0300

    orders

---
+++ src/types/index.ts
@@ -0,0 +1,39 @@
+
+
+export interface Order {
+    id: string;
+    customerId: number;
+    customerName: string;
+    status: string;
+    address: string;
+    estimatedDelivery: string;
+    items: string[];
+    location: {
+      lat: number;
+      lng: number;
+    };
+  }
+
+ export interface User {
+    id: number;
+    name: string;
+    username: string;
+    email: string;
+    address: {
+      street: string;
+      suite: string;
+      city: string;
+      zipcode: string;
+      geo: {
+        lat: string;
+        lng: string;
+      };
+    };
+    phone: string;
+    website: string;
+    company: {
+      name: string;
+      catchPhrase: string;
+      bs: string;
+    };
+  }
