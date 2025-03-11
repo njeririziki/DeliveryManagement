9a5679d43f52d735de7100dac01daff93beecd84
njeririziki <rizikinjeri@gmail.com>
2025-03-11 14:24:47 +0300

    cleanup

--- src/context/ErrorHandlingContext.tsx
+++ src/context/ErrorHandlingContext.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useState, ReactNode, FC,useCallback } from "react";
+import { createContext, useContext, useState, ReactNode, FC,useCallback } from "react";
 
 // Define the shape of the error context
 interface ErrorContextType {
