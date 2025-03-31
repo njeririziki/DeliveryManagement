acaf9fc72335ccb0b445082703c11d0d023770cf
njeririziki <rizikinjeri@gmail.com>
2025-03-31 21:30:58 +0300

    custm order

--- src/pages/OrderDetails.tsx
+++ src/pages/OrderDetails.tsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Descriptions, Skeleton, Typography } from "antd";
+import {  Skeleton, Typography } from "antd";
 import { Order , Feature } from "../types";
 import Map from "../components/map/Map";
 import { useQuery } from "@tanstack/react-query";
 import { fetchSpecificOrderData } from "../services/OrderService";
 import { useError } from "../context/ErrorHandlingContext";
-import CustomCollapse from "../components/custom/CustomCollapse";
-import Package from "../assets/package.svg";
+
 import userAvatars from "../data/userAvatars";
+import OrderCollapseComponent from "../components/reusable/OrderCollapse";
 
 const OrderDetails: React.FC = () => {
   const id = Number(sessionStorage.getItem("orderId"));
@@ -49,39 +49,11 @@ const OrderDetails: React.FC = () => {
     <div className="w-full lg:w-1/3">
       <Typography.Title level={5}>Order : {order.shipmentId} </Typography.Title>
 
-      <CustomCollapse
-        defaultOpen={true}
-        topPart={
-          <div className="flex flex-row  items-center gap-4">
-            <img src={Package} className="bg-gray-100 rounded-full p-2" />
-            <div className="flex flex-col ">
-              <p>Shipment ID </p>
-              <p className="font-semibold"> {order.shipmentId}</p>
-            </div>
-          </div>
-        }
-      >
-        <div className=" flex border-t border-gray-200 pt-4 flex-row  ">
-          <Descriptions title="Order Info" column={1}>
-            <Descriptions.Item label="Estimated Delivery">
-              {order.estimatedDelivery}
-            </Descriptions.Item>
-            <Descriptions.Item label="Customer">
-              {order.customerName}
-            </Descriptions.Item>
-            <Descriptions.Item label="Address">
-              {order.address}
-            </Descriptions.Item>
-            <Descriptions.Item label="Items">
-              {order.items.join(", ")}
-            </Descriptions.Item>
-          </Descriptions>
-          <div className="text-xs bg-green-50 text-green-500 border border-green-300 p-2 h-fit text-nowrap rounded-full ">
-            {" "}
-            {order.status}
-          </div>
-        </div>
-      </CustomCollapse>
+      <OrderCollapseComponent
+      order={order}
+      openOrder={true}
+      actions={<div></div>}
+      />
     </div>
     
     {ordersMapData && <Map type='singleLine' data={ordersMapData}/>}
